6af9e85b6fdd0a8d2b539a82ef4fa5a8cf39ea06
Barinade <poco0317@users.noreply.github.com>
2021-06-11 20:06:31 -0500

    init

---
+++ src/main/java/moe/everything/scribe/reader/ScribeReaderApplication.java
@@ -0,0 +1,13 @@
+package moe.everything.scribe.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribeReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScribeReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moe/everything/scribe/reader/ScribeReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.everything.scribe.reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScribeReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
