52a33e6a544a3d6fb6dafb8cc7b1076a36aa591c
Barinade <poco0317@users.noreply.github.com>
2021-06-11 21:37:34 -0500

    fix component scanning

--- src/main/java/moe/everything/scribe/reader/ScribeReaderApplication.java
+++ src/main/java/moe/everything/scribe/reader/ScribeReaderApplication.java
@@ -2,8 +2,10 @@ package moe.everything.scribe.reader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("moe.everything")
 public class ScribeReaderApplication {
 
 	public static void main(String[] args) {
